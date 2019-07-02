09912b8b84b173148dbd92a79382a6dd7ffef004
Kalliganna <kalligan.naidoo@derivco.co.za>
2019-07-02 19:52:07 +0200

    SignalR kata

---
+++ wschu/wschu/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace wschu
+{
+  public class ChatHub :Hub
+  {
+    public Task JoinGroup(string groupName)
+    {
+      return Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+    }
+
+    public Task SendMessageToGroup(string user, string groupName, string message)
+    {
+      return Clients.Group(groupName).SendCoreAsync("ReceiveGroupMessage", new object[]{ $"{user} : {message} \n"});
+    }
+  }
+}
